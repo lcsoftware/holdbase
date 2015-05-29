09d7906f69dc33678663eada6f679312e8274d77
lcsoftware <lcsoftware@126.com>
2015-05-29 22:28:47 +0800

    auto

---
+++ web-server/public/modules/core/services/common.client.service.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('core').factory('hbHttp', ['$http', function($http) {
+    var service = {};
+
+    service.post = function(url, param, success, err) {
+        $http.post(url, param).success(function(data) {
+            if (success) success(data);
+        }).error(function(data){
+        	if (err) err(data);
+        });
+    }
+
+    return service;
+}]);
